a42777b2135d50ae08c139d9a4e535377275bb0c
Camtendo <ccrockrom@gmail.com>
2018-07-28 17:53:47 -0500

    Refine pathfinder

--- public/scripts/path-finder.js
+++ public/scripts/path-finder.js
@@ -34,5 +34,30 @@ function resolvePathResult(response) {
         $('.path-finder-target').text(`No path exists from ${$('.path-finder-start').val() || 'EMPTY'} to ${$('.path-finder-end').val() || 'EMPTY'}`);
         return;
     }
-    $('.path-finder-target').text(response.join(' => '));
+
+    var zoneNameList = [];
+    for (let i = 0; i < response.length; i++) {
+        if (i+1 >= response.length) {
+            break;
+        }
+
+        var node = getNode(mapState, response[i]);
+        var zone = node.zones.filter(z => {return z.destination === response[i+1];});
+        zoneNameList.push(zone[0].id);
+    }
+
+    var newArray = response.map((d, index) => {
+        if (index >= zoneNameList.length) {
+            return d;
+        }
+
+        return `${d} use ${zoneNameList[index]}`;
+    });
+
+    var orderedList = newArray.map(i => {
+        return `<li>${i}</li>`;
+    })
+
+    const html = `<ol>${orderedList.join('')}</ol>`;
+    $('.path-finder-target').html(html);
 }
